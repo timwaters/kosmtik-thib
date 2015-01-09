a3f2a5582f4d999495720476e3c1cb38e8c1c06d
tcheymol <thibaut.cheymol@eleves.ec-nantes.fr>
2015-01-09 16:27:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,3 @@ exports.Plugin = function (config) {
 var patchConfig = function (e) {
     e.options.compareUrl = e.project.mml.compareUrl ||this.userConfig.compareUrl || 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 };
-/*
-var printProvider = L.print.provider({
-	   method: 'GET',
-	   url: ' ../mapfish-print-development',
-	   autoLoad: true,
-	   dpi: 90
-	});
-
-var printControl = L.control.print({
-	   provider: printProvider
-	});    
-	map.addControl(printControl);
-*/
