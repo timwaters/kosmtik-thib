44455fda63235015e7a3eb0be851b6f56caa3aa9
thibaut <thibaut.cheymol@eleves.ec-nantes.fr>
2015-01-11 10:46:22 +0100

    Commit index.js function buttonAction initialised

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ exports.Plugin = function (config) {
     config.addJS('/node_modules/kosmtik-thib/front.js');
     config.addCSS('/node_modules/kosmtik-thib/front.css');
     config.on('project:tofront', patchConfig);
+    config.on('server:init', function () {
+                                            config.server.addProjectRoute('/exportPdf', buttonAction);});
+    console.log("PouetPouet");
+/*
+    config.commands.export = config.opts.command('exportPdf').help('Export a project');
+    config.on('command:exportPdf', this.buttonAction);*/
+
 
 };
 
 var patchConfig = function (e) {
     e.options.compareUrl = e.project.mml.compareUrl ||this.userConfig.compareUrl || 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 };
-/*
-var printProvider = L.print.provider({
-	   method: 'GET',
-	   url: ' ../mapfish-print-development',
-	   autoLoad: true,
-	   dpi: 90
-	});
 
-var printControl = L.control.print({
-	   provider: printProvider
-	});    
-	map.addControl(printControl);
-*/
+buttonAction = function  (req, res, project) {
+    console.log("PouetPouet");
+    res.writeHead(200, {
+                           
+                        });
+    res.write('PouetPouet');
+    res.end();
+};
